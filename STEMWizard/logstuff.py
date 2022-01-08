1ed2e5cfd6051bfd7faa8c29cdf059ff792af6ed
trice <rtphokie@gmail.com>
2022-01-08 12:31:50 -0500

    added log rotation

--- STEMWizard/logstuff.py
+++ STEMWizard/logstuff.py
@@ -1,15 +1,18 @@
 import logging
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
+from logging.handlers import TimedRotatingFileHandler
 
 
-def get_logger(domain):
-    Log_Format = "%(levelname)s %(asctime)s - %(message)s"
-
-    logging.basicConfig(filename = f"stemwizard_{domain}.log",
-                        # filemode = "w",
-                        format = Log_Format,
-                        level = logging.DEBUG)
-
-    logger = logging.getLogger()
+def get_logger(domain, level=logging.DEBUG):
+    path=f"stemwizard_{domain}.log"
+    if '.log' not in path:
+        path += '.log'
+    logger = logging.getLogger(path)
+    logger.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s [%(filename)15s:%(lineno)3s - %(funcName)15s] %(levelname)-8s %(message)s")
+    logHandler = TimedRotatingFileHandler(path, when='D', interval=1, backupCount=7)
+    logHandler.setLevel(level)
+    logHandler.setFormatter(formatter)
+    logger.addHandler(logHandler)
     return logger
